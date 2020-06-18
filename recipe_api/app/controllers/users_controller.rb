0090b63017a5e9098144e02b80d8e1d6e995c6e1
hawleywoo <alhawley21@gmail.com>
2020-06-18 13:28:14 -0600

    Style User Name Select

--- recipe_api/app/controllers/users_controller.rb
+++ recipe_api/app/controllers/users_controller.rb
@@ -1,2 +1,14 @@
 class UsersController < ApplicationController
+
+    def index
+        # @users = User.all
+        # render json: @users, include: [:recipes]
+        if params["id"]
+            @users = User.where(id: params["id"])
+            render json: @users, include: [:recipes] 
+        else
+            @users = User.all
+            render json: @users, include: [:recipes]
+        end
+    end
 end
